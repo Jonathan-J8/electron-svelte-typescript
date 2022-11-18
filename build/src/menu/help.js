75c0c412a29eaa6078299bc569485bd0267d19e6
jonathan <jonathan@jeudi8.io>
2022-11-18 20:38:57 +0100

    remove build folder

--- build/src/menu/help.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var template = {
-    label: 'Help',
-    role: 'help',
-    submenu: [],
-};
-exports.default = template;
--- build/src/menu/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-var edit_1 = tslib_1.__importDefault(require("./edit"));
-var view_1 = tslib_1.__importDefault(require("./view"));
-var window_1 = tslib_1.__importDefault(require("./window"));
-var help_1 = tslib_1.__importDefault(require("./help"));
-var template = [edit_1.default, view_1.default, window_1.default, help_1.default];
-exports.default = template;
